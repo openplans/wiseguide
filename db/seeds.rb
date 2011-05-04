76749fc09ed5b0ca8355ae4ef61269f62ce8e831
Kevin Chambers <kc@kevinchambers.com>
2011-05-04 16:48:01 -0700

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+require 'csv'
+
+['Successful','Unsuccessful','Incomplete','Exited'].each do |d|
+  Disposition.find_or_create_by_name(d)
+end
+
+['STF','JARC Clackamas','JARC Multnomah','JARC Washington'].each do |fs|
+  FundingSource.find_or_create_by_name(fs)
+end
+
+['Caucasian','African American','Asian','Asian Indian','Chinese','Filipino','Japanese','Korean','Vietnamese','Pacific Islander','American Indian/Alaska Native','Native Hawaiian','Guamanian or Chamorrow','Samoan','Russian','Unknown','Refused','Other'].each do |e|
+  Ethnicity.find_or_create_by_name(e)
+end 
+
+['Initial Interview','Route & Scout','Travel Training','Trainee Shadow'].each do |et|
+  EventType.find_or_create_by_name(et)
+end
+
+CSV.foreach(File.join(Rails.root,'db','seeds','impairments.csv'),headers: true) do |r|
+  Impairment.find_or_create_by_name(r['name'])
+end
+
+CSV.foreach(File.join(Rails.root,'db','seeds','routes.csv'),headers: true) do |r|
+  Route.find_or_create_by_name(r['name'])
+end
+
+CSV.foreach(File.join(Rails.root,'db','seeds','referral_types.csv'),headers: true) do |r|
+  ReferralType.find_or_create_by_name(r['name'])
+end
