491bee4fed3debaa6eb440246de97b87ab525427
Julia West <juliamae@gmail.com>
2011-05-20 12:06:45 -0400

    Add route link shows the route form on the case screen.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  
+  // toggle_form links toggle the next form
+  $(".toggle_form").live("click", function(e){
+    var link = $(this).hide();
+    link.next("form").show();
+    
+    e.preventDefault();
+  });
+});
