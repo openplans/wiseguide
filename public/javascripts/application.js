1c115668f93f65f4327f0feef85cf91799809e6e
Julia West <juliamae@gmail.com>
2011-05-20 17:32:45 -0400

    Adding datepickers to case page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 $(function(){
   
   $('#flash a.closer').click(function() {
-      $('#flash').animate({ height: 0, opacity: 0, marginTop: "-10px", marginBottom: "-10px" }, 'slow');
-      $('#flash a.closer').hide();
-      return false;
+    $('#flash').animate({ height: 0, opacity: 0, marginTop: "-10px", marginBottom: "-10px" }, 'slow');
+    $('#flash a.closer').hide();
+    return false;
   });
   
   // toggle_form links toggle the next form
@@ -12,5 +12,12 @@ $(function(){
     link.next("form").show();
     
     e.preventDefault();
+  });
+  
+  // date picker
+  $('.datepicker').datepicker({
+    showOn: "button",
+    buttonText: "Select",
+    dateFormat: 'yy-mm-dd' 
   });
 });
