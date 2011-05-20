9fdb4e017696e0605e1d234d9a7386196b5e43eb
Julia West <juliamae@gmail.com>
2011-05-20 13:50:55 -0400

    Adding js to close flash notices.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,11 @@
 $(function(){
   
+  $('#flash a.closer').click(function() {
+      $('#flash').animate({ height: 0, opacity: 0, marginTop: "-10px", marginBottom: "-10px" }, 'slow');
+      $('#flash a.closer').hide();
+      return false;
+  });
+  
   // toggle_form links toggle the next form
   $(".toggle_form").live("click", function(e){
     var link = $(this).hide();
