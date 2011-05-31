9410a884d552389bf3ab65315e3799ae6d6182f5
Kevin Chambers <kc@kevinchambers.com>
2011-05-31 15:36:06 -0700

    Legacy data import: phones numbers & email

--- db/legacy.rb
+++ db/legacy.rb
@@ -4,15 +4,18 @@ require 'pp'
 
 PHONE_TYPES = {'Home Phone' => 'Home','Cell Phone' => 'Cell','Business Phone'=>'Business','Business Phone 2'=>'Business','Other' => 'Other','Email - Home' => 'Home Email','Email - Business' => 'Business Email','EMERGENCY Phone' => 'Emergency','EMERGENCY Phone 2' => 'Emergency','TTY' => 'TTY'}
 PHONE_ORDER = {'Home' => 1, 'Cell' => 2, 'Business' => 3, 'Other' => 4, 'Emergency' => 5, 'TTY' => 6}
+EMAIL_ORDER = {'Home Email' => 1, 'Business Email' => 2}
 
 i = 0
 unknown_ethnicity = Ethnicity.find_by_name('Unknown')
 bad_ethnicities = []
-notes = []
+notes = {}
 
+if false
 Customer.record_timestamps = false
 CSV.foreach(File.join(Rails.root,'db','legacy','ridewise_customers.csv'),headers: true) do |r|
-  c = Customer.find_or_initialize_by_id(r['Constituent ID'])
+  id = r['Constituent ID']
+  c = Customer.find_or_initialize_by_id(id)
   c.first_name = r['First Name']
   c.last_name = r['Last Name']
   c.gender = r['Gender']
@@ -23,16 +26,19 @@ CSV.foreach(File.join(Rails.root,'db','legacy','ridewise_customers.csv'),headers
   c.zip = r['Preferred ZIP']
 
   ethnicity_comment = r['Constituent Specific Attributes Ethnicity Comments']
-  notes << "Ethnicity comment: #{ethnicity_comment}" if ethnicity_comment.present?
+  if ethnicity_comment.present?
+    notes[id] ||= [] 
+    notes[id] << "Ethnicity comment: #{ethnicity_comment}" 
+  end
 
   ethnicity_in = r['Constituent Specific Attributes Ethnicity Description']
   ethnicity = Ethnicity.find_by_name(ethnicity_in)
   if ethnicity.nil? 
-    ethnicity = Ethnicity.find_by_name()
+    # ethnicity = Ethnicity.find_by_name()
     ethnicity = unknown_ethnicity 
     bad_ethnicities << ethnicity_in unless bad_ethnicities.include?(ethnicity_in)
-    c.ethnicity = ethnicity
   end
+  c.ethnicity = ethnicity
   
   c.created_at = fix_up_date(r['Constituent Date Added'])
   c.updated_at = fix_up_date(r['Constituent Date Last Changed'])
@@ -43,6 +49,7 @@ CSV.foreach(File.join(Rails.root,'db','legacy','ridewise_customers.csv'),headers
 end
 ActiveRecord::Base.connection.execute("SELECT setval('customers_id_seq',#{Customer.maximum(:id)})")
 Customer.record_timestamps = false
+end
 
 cc = {}
 CSV.foreach(File.join(Rails.root,'db','legacy','ridewise_customers.csv'),headers: true) do |r|
@@ -51,19 +58,41 @@ CSV.foreach(File.join(Rails.root,'db','legacy','ridewise_customers.csv'),headers
   metatype = (type =~ /email/i ? :email : :phone)
   phone = r['Preferred Phone Number']
   if phone.present?
-    cc[id] = {} unless cc.key?(id)
-    cc[id][metatype] = {} unless cc[id].key?(metatype)
+    cc[id] ||= {} 
+    cc[id][metatype] ||= {} 
     cc[id][metatype][type] = phone 
   end
 end
 
 cc.each do |key, value|
+  c = Customer.find(key)
   if value.key?(:phone)
     phones = value[:phone].to_a
     phones.sort! {|x,y| PHONE_ORDER[x[0]] <=> PHONE_ORDER[y[0]]}
-    PP.pp phones
+    c.phone_number_1 = phones[0].join(': ')
+    c.phone_number_2 = phones[1].join(': ') if phones.size > 1
+    if phones.size > 2
+      phones[2..-1].each do |phone|
+        notes[key] ||= []
+        notes[key] << phone.join(': ')
+      end
+    end
   end
+  if value.key?(:email)
+    emails = value[:email].to_a
+    emails.sort! {|x,y| EMAIL_ORDER[x[0]] <=> EMAIL_ORDER[y[0]]}
+    c.email = emails[0][1]
+    if emails.size > 1
+      emails[1..-1].each do |email|
+        notes[key] ||= []
+        notes[key] << email.join(': ')
+      end
+    end
+  end
+  c.notes = notes[key].join("\n") unless notes[key].nil?
+  c.save!
 end
+
 
 #cc.each_value do |c|
 #  PP.pp c[:phone] if c.key?(:phone) && c[:phone].size > 2
