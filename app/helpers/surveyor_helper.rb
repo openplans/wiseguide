5effa36606319580a541d92fbb1dce359b497c22
Julia West <juliamae@gmail.com>
2011-06-24 14:35:42 -0400

    Displaying survey answers with multiple selections on one line.

--- app/helpers/surveyor_helper.rb
+++ app/helpers/surveyor_helper.rb
@@ -1,21 +1,20 @@
 module SurveyorHelper
   def display_response(r_set,question)
-    sets = r_set.responses.select{|r| r.question == question && r.question.display_order == question.display_order}
-      if sets.size == 0
-        return "-"
-      elsif sets.size == 1
-        return (sets.first.string_value || sets.first.text_value || sets.first.integer_value || sets.first.float_value || show_answer(sets.first)).to_s
-      else
-        txt = ""
-        sets.each do |set|
-          txt << show_answer(set) + "<br/>"
-        end
-        return txt
-      end
+    sets = r_set.responses.select do |r| 
+      r.question == question && r.question.display_order == question.display_order
+    end
+    
+    if sets.size == 0
+      "-"
+    elsif sets.size == 1
+      (sets.first.string_value || sets.first.text_value || sets.first.integer_value || sets.first.float_value || show_answer(sets.first)).to_s
+    else
+      sets.map { |s| show_answer(s) }.join( question.pick == "any" ? ";" : "<br/>" )
+    end
   end
   
-  def show_answer(set)
-     set.answer.text
+  def show_answer(set)  
+    set.answer.text
   end
 
   # Layout: stylsheets and javascripts
