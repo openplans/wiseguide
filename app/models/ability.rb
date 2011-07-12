aef32827f952161643f443d52c6545d0674f3064
Kevin Chambers <kc@kevinchambers.com>
2011-07-12 14:52:41 -0700

    fix missing authorization for support network members

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -32,8 +32,9 @@ class Ability
     can ability, Outcome
     can ability, ResponseSet
     can ability, Survey
+    can ability, CustomerSupportNetworkMember
 
-    #users can only read their own cases
+    #users can only read the cases of others
     can :read, Contact
     can :manage, Contact, :user_id == user.id
 
